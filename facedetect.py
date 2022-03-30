318c86fbcfb2e804f270903d792fa760c2d801b2
Nieliton Rodrigues <nieliton.sousa3040@gmail.com>
2022-03-30 19:47:41 -0300

    My first detected face

---
+++ facedetect.py
@@ -0,0 +1,29 @@
+import cv2
+
+xml_haarcascade = "haarcascade_frontalface_alt2.xml"
+xml_eye = "haarcascade_eye.xml"
+
+faceClassifier = cv2.CascadeClassifier(xml_haarcascade)
+eyeClassifier = cv2.CascadeClassifier(xml_eye)
+
+capture = cv2.VideoCapture(0)
+
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+while not cv2.waitKey(20) & 0xFF == ord("q"):
+    ret, frame_color = capture.read()
+
+    gray = cv2.cvtColor(frame_color, cv2.COLOR_BGR2GRAY)
+
+    faces = faceClassifier.detectMultiScale(gray)
+    
+    eyes = eyeClassifier.detectMultiScale(gray)
+
+    for x, y, w, h in faces:
+        cv2.rectangle(frame_color, (x, y), (x + w, y + h), (0,255,0), 2)
+
+    for x, y, w, h in eyes:
+        cv2.rectangle(frame_color, (x, y), (x + w, y + h), (0,255,0), 2)
+
+    cv2.imshow('color', frame_color)
